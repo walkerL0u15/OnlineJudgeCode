0fe055df9cd1d1c14c58d9432fceb1f0c0408133
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2020-04-24 13:12:30 +0800

    Add files via upload

---
+++ e975.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+	int min=1000;
+	bool f;
+	string g="love",g2="Love",s,ss;
+	getline(cin,s);
+	for(int i=0;i<s.size();++i){
+		ss="";
+		bool f=true;
+		for(int j=i;j<i+4;++j)
+			if((s[j]>='a'&&s[j]<='z')||(s[j]>='A'&&s[j]<='Z'))
+				f=false;
+		if(!f)
+			continue;
+		for(int j=i;j<i+4;++j)
+			ss+=s[j];
+		for(int j=0;j<26;++j){
+			for(int k=0;k<4;++k){
+				ss[k]+=1;
+				if(ss[k]>'Z'||ss[k]>'z')
+					ss[k]-=26;
+			}
+			cout<<ss;
+			if(ss==g||ss==g2){
+				if(min>j)
+					min=j;
+				break;
+			}
+		}
+	}
+	printf("%d\n",min);
+	return 0;
+}
