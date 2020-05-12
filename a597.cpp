f6ec544c6133e11b1077f6e7473458ff2985f589
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2020-05-12 17:07:45 +0800

    Update a597.cpp

--- a597.cpp
+++ a597.cpp
@@ -9,7 +9,10 @@ bool floor[500][500];
 int main()
 {
 	int r,c;
+	int r[4]={1,0,-1,0};
+	int c[4]={0,-1,0,1};
 	string s;
+	P t;
 	queue<P> b;
 	memset(floor,false,sizeof(floor));
 	scanf(" %d %d",&r,&c);
@@ -24,7 +27,12 @@ int main()
 			if(floor[i][j])
 				b.push(P(i,j));
 			while(!b.empty()){
-				
+				t=b.front();
+				if(floor[t.first][t.second]==false||t.first>=r||t.second>=c){
+					b.pop();
+					continue;
+				}
+				floor[t.first][t.second]=false;
 			}
 		}
 }
