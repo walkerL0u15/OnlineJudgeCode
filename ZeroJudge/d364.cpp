6164961b774225588dbf304fddb80169af1fc48d
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2020-05-26 16:11:47 +0800

    Update d364.cpp

--- ZeroJudge/d364.cpp
+++ ZeroJudge/d364.cpp
@@ -1,38 +1,52 @@
-#include<iostream>
-using namespace std;
+#include<stdio.h>
 
-int ans[32];
-int N,s,t;
+int S,t,num[31];
+bool flag;
 bool gcd(int a,int b){
-	while((a%=b)!=0&&(b%=a)!=0);
-	return ((a==1||b==1)?true:false);
+    int temp;
+    while(b!=0){
+        temp=a%b;
+        a=b;
+        b=temp;
+    }
+    if(a==1)
+        return true;
+    return false;
 }
-void check(int counter,int left,int i){
-	if(left<ans[counter-1])
-		return;
-	else if(counter>=t&&gcd(ans[counter-1],left)){
-		for(int j=1;j<t;++j)
-			printf("%d ",ans[j]);
-		printf("%d\n",left);
-		return;
-	}
-	for(;i<=left;++i)
-		if(gcd(i,ans[counter-1])){
-			ans[counter]=i;
-			check(counter+1,left-i,i);
-		}
-	return;
+void dfs(int use,int left){
+    if(use>=t+1){
+        if(left==0){
+            for(int i=1;i<use;++i)
+                printf("%d ",num[i]);
+            puts("");
+        }
+        return;
+    }
+    for(int i=num[use-1];i<=left;++i){
+        flag=true;
+        for(int j=1;j<use;++j)
+            if(!gcd(i,num[j])){
+                flag=false;
+                break;
+            }
+        if(flag){
+            num[use]=i;
+            dfs(use+1,left-i);
+        }
+    }
+    return;
 }
-
 
 int main()
 {
-	scanf("%d",&N);
-	ans[0]=1;
-	for(int i=1;i<=N;++i){
-		scanf("%d%d",&s,&t);
-		printf("Case %d:\n",i);
-		check(1,s,1);
-	}
-	return 0;
+    int N;
+    num[0]=1;
+    while(scanf(" %d",&N)!=EOF){
+        for(int i=1;i<=N;++i){
+            scanf(" %d %d",&S,&t);
+            printf("Case %d:\n",i);
+            dfs(1,S);
+        }
+    }
+    return 0;
 }
