a8482675519a0662b2ed0a3a0b7f2b2ccf142553
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2020-09-27 19:07:57 +0800

    Update 2385.cpp

--- POJ/2385.cpp
+++ POJ/2385.cpp
@@ -1,34 +1,25 @@
-#include<stdio.h>
-#include<string.h>
+#include<cstdio>
+#include<cstring>
 #include<algorithm>
 using namespace std;
 
-int main()
-{
-    int T,W,dp[1003][32],drop[1003];
-    memset(dp,0,sizeof(dp));
-    scanf(" %d %d",&T,&W);
-    for(int i=1;i<=T;++i){
-        scanf(" %d",&drop[i]);
-        drop[i]%=2;
-    }
-    if(drop[1]==1) dp[1][0]=1,dp[1][1]=0;
-    else dp[1][0]=0,dp[1][1]=1;
-    for(int i=2;i<=T;++i)
-        for(int j=0;j<=i;++j){
-            if(j==0)
-                dp[i][j]=dp[i-1][j]+drop[i];
-            else{
-                dp[i][j]=max(dp[i-1][j],dp[i-1][j-1]);
-                if(j%2==1&&drop[i]==0)
-                    ++dp[i][j];
-                else if(j%2==0&&drop[i]==1)
-                    ++dp[i][j];
-            }
-        }
-    int ans=-1;
-    for(int i=0;i<=W;++i)
-        ans=max(ans,dp[T][i]);
-    printf("%d\n",ans);
-    return 0;
+int main(){
+	int T,W,dp[2][1005],ans=-1;
+	int apple[2][1005];
+	scanf(" %d %d",&T,&W);
+	for(int i=1;i<=T;++i){
+		scanf(" %d",&apple[0][i]);
+		--apple[0][i];
+		apple[1][i]=!apple[0][i];
+	}
+	for(int k=0;k<=1;++k){
+		memset(dp,0,sizeof(dp));
+		for(int i=k&1;i<=W+k&1;++i){
+			for(int j=1;j<=T;++j)
+				dp[i&1][j]=max(dp[!(i&1)][j-1],dp[i&1][j-1])+apple[i&1][j];
+			ans=max(ans,dp[i&1][T]);
+		}
+	}
+	printf("%d\n",max(ans,T-ans));
+	return 0;
 }
