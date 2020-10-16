a0b0331f31b64f0545527a71c05d60567457db71
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2020-10-16 13:10:27 +0800

    Update 3046.cpp

--- NotFinish/3046.cpp
+++ NotFinish/3046.cpp
@@ -1,21 +1,27 @@
 #include<cstdio>
 #include<cstring>
+#define MOD (int)1e7
 
-int dp[100005][2];
+int dp[2][100005];
 
 int main(){
-    int F,A,S,B,ant;
+    int F,A,S,B,ant[1005],ans;
     scanf(" %d %d %d %d",&F,&A,&S,&B);
     memset(dp,0,sizeof(dp));
-    for(int i=0;i<F;++i){
+    memset(ant,0,sizeof(ant));
+    dp[0][0]=dp[1][0]=1;
+    for(int i=1;i<=A;++i){
         scanf(" %d",&ant);
         for(int j=1;j<=B;++j){
             if(j-1>=ant)
-                dp[i&1][j]=dp[!(i&1)][j]+(dp[i&1][j-1]-dp[i&1][j-1-ant])-(dp[!(i&1)][j-1]-dp[!(i&1)][j-1-ant]);
+                dp[i%2][j]=(dp[i%2][j-1]+dp[(i-1)%2][j]-dp[(i-1)%2][j-ant-1]+MOD)%MOD;
             else
-                dp[i&1][j]=dp[!(i&1)][j]+dp[i&1][j-1]-dp[!(i&1)][j-1];
+                dp[i%2][j]=(dp[i%2][j-1]+dp[(i-1)%2][j]+MOD)%MOD;
         }
     }
-    printf("%d\n",dp[!(F&1)][B]-dp[!(F&1)][S-1]);
+    ans=0;
+    for(int i=S;i<=B;++i)
+    	ans+=dp[A%2][i];
+    printf("%d\n",ans);
     return 0;
 }
