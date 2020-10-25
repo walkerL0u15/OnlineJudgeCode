377edf33daed9eec6f2f6c372ce3b49391dfb2ad
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2020-10-25 20:51:59 +0800

    Update 2236.cpp

--- POJ/2236.cpp
+++ POJ/2236.cpp
@@ -1,55 +1,59 @@
-#include<stdio.h>
-#include<vector>
-using namespace std;
-#define MaxN 1002
-int par[MaxN],dep[MaxN];
+#include<cstdio>
+const int MaxN=1005;
+
+int deep[MaxN],par[MaxN];
 
 void init(int n){
-	for(int i=1;i<=n;++i)
-		par[i]=i,dep[i]=0;
-	return;
+    for(int i=0;i<=n;++i)
+        deep[i]=0,par[i]=i;
+    return;
 }
-int find(int x){
-	if(par[x]==x) return x;
-	return par[x]=find(par[x]);
+int find(int a){
+    if(par[a]==a)
+        return a;
+    return par[a]=find(par[a]);
 }
-void unite(int x,int y){
-	x=find(x),y=find(y);
-	if(x==y) return;
-	if(dep[x]>dep[y]) par[y]=x;
-	else{
-		par[x]=y;
-		if(dep[x]==dep[y]) ++dep[y];
-	}
-	return;
+void unite(int a,int b){
+    int fa=find(a),fb=find(b);
+    if(fa==fb) return;
+    if(deep[fa]>deep[fb])
+        par[fb]=fa;
+    else{
+        par[fa]=fb;
+        if(deep[fa]==deep[fb]) ++deep[fb];
+    }
+    return;
 }
-bool same(int x,int y){
-	return find(x)==find(y);
+struct XY{
+    int x,y;
+}dir[MaxN];
+
+int dis(XY a,XY b){
+    return (a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y);
 }
 
 int main(){
-	int N,D,corx[1002],cory[1002],x,y,a,b;
-	vector<int> f;
-	char w;
-	scanf(" %d %d",&N,&D);
-	init(N);
-	for(int i=1;i<=N;++i)
-		scanf(" %d %d",&corx[i],&cory[i]);
-	while(scanf(" %c",&w)==1){
-		if(w=='S'){
-			scanf(" %d %d",&a,&b);
-			if(same(a,b)) puts("SUCCESS");
-			else puts("FAIL");
-		}
-		else{
-			scanf(" %d",&a);
-			for(int i=0;i<f.size();++i){
-				x=corx[f[i]]-corx[a],y=cory[f[i]]-cory[a];
-				if(x*x+y*y<=D*D)
-					unite(f[i],a);
-			}
-			f.push_back(a);
-		}
-	}
-	return 0;
+    int N,d,fix[MaxN],cnt=0,xx,yy;
+    char g;
+    scanf(" %d %d",&N,&d);
+    init(N);
+    for(int i=1;i<=N;++i)
+        scanf(" %d %d",&dir[i].x,&dir[i].y);
+    while(scanf(" %c",&g)==1){
+        if(g=='O'){
+            scanf(" %d",&xx);
+            for(int i=0;i<cnt;++i)
+                if(dis(dir[fix[i]],dir[xx])<=d*d)
+                    unite(fix[i],xx);
+            fix[cnt++]=xx;
+        }
+        else if(g=='S'){
+            scanf(" %d %d",&xx,&yy);
+            if(find(xx)==find(yy))
+                puts("SUCCESS");
+            else
+                puts("FAIL");
+        }
+    }
+    return 0;
 }
