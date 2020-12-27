0c1aa011fc4b70663ecc8f97dfab65bfe019129d
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2020-12-27 22:12:40 +0800

    Create 3061.cpp

---
+++ POJ/3061.cpp
@@ -0,0 +1,31 @@
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+
+int num[100005];
+
+int main(){
+	int T;
+	int N,S;
+	scanf(" %d",&T);
+	while(T--){
+		scanf(" %d %d",&N,&S);
+		for(int i=0;i<N;++i)
+			scanf(" %d",&num[i]);
+		int ans=1e8;
+		int s=0,t=0,total=num[0];
+		while(s<N){
+			while(t<N-1&&total<S)
+				total+=num[++t];
+			if(total<S) break;
+			ans=min(ans,t-s+1);
+			total-=num[s];
+			++s;
+		}
+		if(ans==1e8)
+			printf("0\n");
+		else
+			printf("%d\n",ans);
+	}
+	return 0;
+}
