98491686b36b42cdda35128b729071c4031470c7
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2020-12-27 20:22:13 +0800

    Update 1064.cpp

--- POJ/1064.cpp
+++ POJ/1064.cpp
@@ -1,22 +1,23 @@
-#include<stdio.h>
-#include<math.h>
+#include<cstdio>
+#include<cmath>
 
 int N,K;
-double r[10003],lb=0,ub=10000000,mid;
-bool cut(double num){
-	int total=0;
+double line[10005];
+bool C(double num){
+	int res=0;
 	for(int i=0;i<N;++i)
-		total+=(int)(r[i]/num);
-	return total>=K;
+		res+=line[i]/num;
+	return res>=K;
 }
 
 int main(){
 	scanf(" %d %d",&N,&K);
 	for(int i=0;i<N;++i)
-		scanf(" %lf",&r[i]);
+		scanf(" %lf",&line[i]);
+	double lb=0,ub=200000;
 	for(int i=0;i<100;++i){
-		mid=(lb+ub)/2;
-		if(cut(mid))
+		double mid=(lb+ub)/2;
+		if(C(mid))
 			lb=mid;
 		else
 			ub=mid;
