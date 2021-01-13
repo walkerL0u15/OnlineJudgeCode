0a5ec5040cd2544879bc1e6ef6613dd873aca9bc
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2021-01-13 13:11:05 +0800

    Update 3684.cpp

--- 3684.cpp
+++ 3684.cpp
@@ -1,19 +1,32 @@
 #include<cstdio>
 #include<cmath>
+#include<algorithm>
+using namespace std;
 
+int N;
 double ballp[105]={0};
-double N,H,R,T,FT;
+double H,R,T,FT;
+double calc(double t){
+    int times=t/FT;
+    double lt=t-FT*(double)times;
+    if(!times&1)
+        lt=FT-lt;
+    return H-5*lt*lt;
+}
 
 int main(){
     int C;
     scanf(" %d",&C);
     while(C--){
-        scanf(" %lf %lf %lf %lf",&N,&H,&R,&T);
+        scanf(" %d %lf %lf %lf",&N,&H,&R,&T);
         R/=100;
         FT=sqrt(2*H/10);
         for(int i=0;i<N;++i){
-            ballp[i]=calc((T-i);
+            ballp[i]=calc(T-i);
         }
+        sort(ballp,ballp+N);
+        for(int i=0;i<N;++i)
+            printf("%.2lf%c",ballp[i]+2*i*R,(i==N-1?'\n':' '));
     }
     return 0;
 }
