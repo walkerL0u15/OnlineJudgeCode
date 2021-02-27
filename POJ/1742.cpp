d7e320d51f9f5674e0289692bb3a89f291dc0ce6
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2021-02-27 20:43:27 +0800

    Update 1742.cpp

--- POJ/1742.cpp
+++ POJ/1742.cpp
@@ -1,35 +1,32 @@
 #include<cstdio>
 #include<cstring>
-#include<algorithm>
-using namespace std;
 
-int dp[(int)1e5+5];
-struct C{
-	int c,num;
-}coin[1005];
+int way[(int)1e5+10];
+int v[105],a[105];
 
 int main(){
-	int N,Max,ans;
-	while(scanf(" %d %d",&N,&Max)==2&&N!=0){
-		memset(dp,-1,sizeof(dp));
-		dp[0]=0;
-		for(int i=1;i<=N;++i)
-			scanf(" %d",&coin[i].c);
-		for(int i=1;i<=N;++i)
-			scanf(" %d",&coin[i].num);
-		for(int i=1;i<=N;++i){
-			for(int j=0;j<=Max;++j){
-				if(dp[j]>=0)
-					dp[j]=coin[i].num;
-				else if(j>=coin[i].c&&dp[j-coin[i].c]>0)
-					dp[j]=dp[j-coin[i].c]-1;
-			}
-		}
-		ans=0;
-		for(int i=1;i<=Max;++i)
-			if(dp[i]>=0)
-				++ans;
-		printf("%d\n",ans);
-	}
-	return 0;
+    int N,M;
+    while(scanf(" %d %d",&N,&M)==2&&N!=0){
+        memset(way,-1,sizeof(way));
+        way[0]=0;
+        for(int i=0;i<N;++i)
+            scanf(" %d",&v[i]);
+        for(int i=0;i<N;++i)
+            scanf(" %d",&a[i]);
+        for(int i=0;i<N;++i) {
+            for (int j = 0; j <= M; ++j) {
+                if (way[j] >= 0)
+                    way[j] = a[i];
+                else if (j - v[i] >= 0 && way[j - v[i]] > 0)
+                    way[j] = way[j - v[i]] - 1;
+            }
+        }
+        int res=0;
+        for(int i=1;i<=M;++i)
+            if(way[i]>=0){
+                ++res;
+            }
+        printf("%d\n",res);
+    }
+    return 0;
 }
