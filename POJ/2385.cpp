a6c65cd0d980bfbf82688532f9a657077d8d5a59
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2021-02-27 19:49:35 +0800

    Update 2385.cpp

--- POJ/2385.cpp
+++ POJ/2385.cpp
@@ -1,25 +1,24 @@
 #include<cstdio>
-#include<cstring>
 #include<algorithm>
 using namespace std;
 
+int dp[2][35]={0},drop;
+
 int main(){
-	int T,W,dp[2][1005],ans=-1;
-	int apple[2][1005];
-	scanf(" %d %d",&T,&W);
-	for(int i=1;i<=T;++i){
-		scanf(" %d",&apple[0][i]);
-		--apple[0][i];
-		apple[1][i]=!apple[0][i];
-	}
-	for(int k=0;k<=1;++k){
-		memset(dp,0,sizeof(dp));
-		for(int i=k&1;i<=W+k&1;++i){
-			for(int j=1;j<=T;++j)
-				dp[i&1][j]=max(dp[!(i&1)][j-1],dp[i&1][j-1])+apple[i&1][j];
-			ans=max(ans,dp[i&1][T]);
-		}
-	}
-	printf("%d\n",max(ans,T-ans));
-	return 0;
+    int T,W;
+    int *now=dp[0],*last=dp[1];
+    scanf(" %d %d",&T,&W);
+    for(int i=1;i<=T;++i){
+        scanf(" %d",&drop);
+        drop=drop-1;
+        now[0]=last[0]+(!drop);
+        for(int j=1;j<=W;++j)
+            now[j]=max(last[j],last[j-1])+(((j+1)&1)^drop);
+        swap(now,last);
+    }
+    int res=-1;
+    for(int i=0;i<=W;++i)
+        res=max(res,last[i]);
+    printf("%d\n",res);
+    return 0;
 }
