8b273277ee36b4407716c9393a016ec343e86f83
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2021-05-18 19:08:19 +0800

    Create 19.cpp

---
+++ SproutOJ/19.cpp
@@ -0,0 +1,27 @@
+#include<cstdio>
+#include<stack>
+using namespace std;
+
+int main(){
+	int C,N,A,B;
+	scanf(" %d",&C);
+	while(C--){
+		stack<int> station;
+		scanf(" %d",&N);
+		A=1;
+		for(int i=0;i<N;++i){
+			scanf(" %d",&B);
+			if(!station.empty()&&station.top()==B)
+				station.pop();
+			else{
+				while(A<=N&&A!=B){
+					station.push(A);++A;
+				}
+				++A;
+			}
+		}
+		if(station.empty()) printf("Yes\n");
+		else printf("No\n");
+	}
+	return 0;
+}
