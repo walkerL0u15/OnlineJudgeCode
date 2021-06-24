aef00451ad52f820da73c2fa947c3718db080067
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2021-06-24 21:46:49 +0800

    Create 48.cpp

---
+++ SproutOJ/48.cpp
@@ -0,0 +1,42 @@
+#include<cstdio>
+
+struct Node{
+    int num;
+    Node *lchild=NULL,*rchild=NULL;
+};
+int x;
+Node *newNode=NULL,*f=NULL;
+void in(int k,Node* now){
+    if(k>now->num){
+        if(now->rchild==NULL)
+            now->rchild=newNode;
+        else
+            in(k,now->rchild);
+    }
+    else{
+        if(now->lchild==NULL)
+            now->lchild=newNode;
+        else
+            in(k,now->lchild);
+    }
+    return;
+}
+void print(Node *now){
+    if(now->lchild!=NULL)
+        print(now->lchild);
+    if(now->rchild!=NULL)
+        print(now->rchild);
+    printf("%d\n",now->num);
+    return;
+}
+
+int main(){
+    scanf(" %d",&x);
+    f=new Node,f->num=x;
+    while(scanf(" %d",&x)!=EOF){
+        newNode=new Node,newNode->num=x;
+        in(x,f);
+    }
+    print(f);
+    return 0;
+}
