f4a6de3105ee202c0760ab9a233f3471b21ee60d
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2021-08-29 22:44:30 +0800

    Update b298.cpp

--- ZeroJudge/b298.cpp
+++ ZeroJudge/b298.cpp
@@ -1,33 +1,36 @@
-#include<iostream>
+#include<cstdio>
 #include<vector>
 using namespace std;
 
-vector<int> fac[10001];
-bool bad[10001];
-bool found(int test){
-	if(bad[test])
-		return true;
-	for(int i=0;i<fac[test].size();++i)
-		if(found(fac[test][i]))
-			return true;
-	return false;
+vector<int> f[10005];
+bool pollute[10005]={0};
+
+void dfs(int cur){
+	pollute[cur]=true;
+	for(int i=0;i<f[cur].size();++i)
+		if(!pollute[f[cur][i]])
+			dfs(f[cur][i]);
+	return;
 }
 
-int main()
-{
-	int n,m,l,q,a,b;
-	scanf("%d%d%d%d",&n,&m,&l,&q);
-	for(int i=0;i<m;++i){
-		scanf("%d%d",&a,&b);
-		fac[b].push_back(a);
+int main(){
+	int N,M,L,Q;
+	scanf(" %d %d %d %d",&N,&M,&L,&Q);
+	int a,b;
+	for(int i=0;i<M;++i){
+		scanf(" %d %d",&a,&b);
+		f[a].push_back(b);
 	}
-	for(int i=0;i<l;++i){
-		scanf("%d",&a);
-		bad[a]=true;
+	for(int i=0;i<L;++i){
+		scanf(" %d",&a);
+		dfs(a);
 	}
-	for(int i=0;i<q;++i){
-		scanf("%d",&a);
-		printf("%s\n",(found(a)?"TUIHUOOOOOO":"YA~~"));
+	for(int i=0;i<Q;++i){
+		scanf(" %d",&a);
+		if(pollute[a])
+			printf("TUIHUOOOOOO\n");
+		else
+			printf("YA~~\n");
 	}
 	return 0;
 }
