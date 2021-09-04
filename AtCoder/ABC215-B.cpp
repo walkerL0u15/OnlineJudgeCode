2f4e625f66049c4965c92ddf2d1c000dd4c784f8
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2021-09-04 15:35:22 +0800

    Create ABC215-B.cpp

---
+++ AtCoder/ABC215-B.cpp
@@ -0,0 +1,14 @@
+//ABC215-B.cpp
+#include<iostream>
+#include<cmath>
+using namespace std;
+typedef long long LL;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	LL n;
+	cin>>n;
+	cout<<floor(log2((long double)n))<<"\n";
+	return 0;
+}
