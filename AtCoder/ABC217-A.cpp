73724c2018ceaed7c614a71d95363a50512abd4c
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2021-09-05 11:40:39 +0800

    Create ABC217-A.cpp

---
+++ AtCoder/ABC217-A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false),cin.tie(0);
+	string A,B;
+	cin>>A>>B;
+	if(A<B)
+		cout<<"Yes\n";
+	else
+		cout<<"No";
+}
