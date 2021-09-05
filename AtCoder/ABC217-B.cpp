4c3f5bfcf440398e0c3638eaffcd8a636f1e05a1
walkerL0u15 <49367744+walkerL0u15@users.noreply.github.com>
2021-09-05 11:53:42 +0800

    Create ABC217-B.cpp

---
+++ AtCoder/ABC217-B.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false),cin.tie(0);
+	char pin[5];
+	cin>>pin;
+	if(pin[0]==pin[1]&&pin[1]==pin[2]&&pin[2]==pin[3]){
+		cout<<"Weak\n";
+		return 0;
+	}
+	for(int i=1;i<=3;++i)
+		if(!(pin[i]-pin[i-1]==1||pin[i]-pin[i-1]==-9)){
+			cout<<"Strong\n";
+			return 0;
+		}
+	cout<<"Weak\n";
+	return 0;
+}
